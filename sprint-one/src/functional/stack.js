199af2c0396058f6a76bf26bb1f0c503a02c2bb9
Pamela Sabella <psabella@Pamelas-Mac.local>
2019-12-11 15:05:59 -0800

    complete stack functional

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,51 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    if (Object.keys(storage).length !== 0) {
+      var keys = Object.keys(storage);
+      var numKeys = keys.map(function(key){
+        return parseInt(key);
+      })
+      var lastKey = numKeys.reduce(function(max, key){
+        if (key > max) {
+          max = key;
+          return max;
+        } else {
+          return max;
+        }
+      });
+      var newKey = lastKey + 1;
+    } else {
+      var newKey = 1;
+    }
+    storage[newKey.toString()] = value;
   };
 
   someInstance.pop = function() {
+    if (Object.keys(storage).length !== 0) {
+      var keys = Object.keys(storage);
+      var numKeys = keys.map(function(key){
+        return parseInt(key);
+      })
+      var lastKey = numKeys.reduce(function(max, key) {
+        if (key > max) {
+          max = key;
+          return max;
+        } else {
+          return max;
+        }
+      });
+      var lastValue = storage[lastKey.toString()];
+      var poppedValue = lastValue;
+      delete storage[lastKey.toString()];
+
+      return poppedValue;
+    }
   };
 
   someInstance.size = function() {
+    var keys = Object.keys(storage);
+    return keys.length;
   };
 
   return someInstance;
