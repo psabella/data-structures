48be0c82768c699691b35b86e2cdd56ea04771b6
Pamela Sabella <psabella@Pamelas-Mac.local>
2019-12-12 08:56:31 -0800

    stub out stack functional-shared

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,26 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  // create new stack object
+  var newStack = {};
+  // add the methods to the new stack object
+  _.extend(newStack, Stack.stackMethods);
+  // return the new stack object
+  return newStack;
 };
 
+// variable to store the methods (object)
 var stackMethods = {};
+
+// add the methods to the class
+Stack.stackMethods.push = function(value) {
+
+};
+
+Stack.stackMethods.pop = function() {
+
+};
+
+Stack.stackMethods.size = function() {
+
+};
 
 
