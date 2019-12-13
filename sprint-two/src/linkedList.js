af07f1d43bba4b3e4839e0a03264aca89d7d3819
psabella <pamsabella@gmail.com>
2019-12-12 17:28:54 -0800

    working on removeHead and addToTail of linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,14 +1,39 @@
 var LinkedList = function() {
   var list = {};
-  list.head = null;
-  list.tail = null;
-
+  list.head = null; // {value: x, next: n}
+  list.tail = null; // {value: n, next: null}
+  // {1: {value: x, next: n}, 2: {value: n, next: n2}}
+  // adds a node to the end of the linked list
   list.addToTail = function(value) {
+    if (list.tail === null) {
+      list.tail = Node(value);
+      list.head = list.tail;
+    } else {
+      var oldTail = list.tail;
+      list.tail = Node(value); // {value: 12, next: null}
+      // list : { 'tail' : {value:12, next:null} }
+      // next from the previous tail should now point to the newNode value
+      // to do that...we must access the previous node and set the next value
+      oldTail.next = list.tail.value;
+    }
   };
 
+  // removes the first node in the linked list
+  // returns the removed value
   list.removeHead = function() {
+    //check head is null or not
+    if(list.head === null) {
+      return undefined;
+    } else {
+      var oldHead = list.head;
+      //if not null, then delete list.head and
+      list.head = list.head.value; // new head
+    }
+    return list.head;
   };
 
+  // checks if the target is in node the linked list
+  // returns boolean
   list.contains = function(target) {
   };
 
@@ -17,6 +42,7 @@ var LinkedList = function() {
 
 var Node = function(value) {
   var node = {};
+  // {value: 12, next: null}
 
   node.value = value;
   node.next = null;
