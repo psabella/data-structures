b72bce1a3f73e53618618e9d0e126eae2eb46627
Pamela Sabella <psabella@Pamelas-Mac.local>
2019-12-16 03:23:41 -0800

    refactored functional shared methods

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -3,6 +3,9 @@ var Queue = function() {
   var newQueue = {};
   // add a storage object to hold int keys + items
   newQueue.storage = {};
+  // keep track of first and last in queue
+  newQueue.startIndex = 0;
+  newQueue.nextIndex = 0;
   // add the methods to the new queue object
   _.extend(newQueue, queueMethods);
   // return the new queue object
@@ -13,67 +16,25 @@ var queueMethods = {};
 
 // enqueue(string) - Add a string to the back of the queue
 queueMethods.enqueue = function(value) {
-  // check if there are any items in the queue
-  if (Object.keys(this.storage).length !== 0) {
-    // get the keys for each item
-    var keys = Object.keys(this.storage);
-    // convert the keys to numbers
-    var numKeys = keys.map(function(key){
-      return parseInt(key);
-    });
-    // find the max number among the keys - the key of the last item
-    var lastKey = numKeys.reduce(function(max, key){
-      if (key > max) {
-        max = key;
-      }
-      return max;
-    });
-    // increment the last key to get the new key
-    var newKey = lastKey + 1;
-  } else {
-    // if there were no items in the stack, the first key will be 1
-    var newKey = 1;
-  }
-  // set the new key to the value
-  this.storage[newKey.toString()] = value;
+  // new value is set at nextIndex in storage
+  this.storage[this.nextIndex] = value;
+  this.nextIndex++;
 };
 
 // dequeue() - Remove and return the string at the front of the queue
 queueMethods.dequeue = function() {
   // check if there are any items in the queue
-  if (Object.keys(this.storage).length !== 0) {
-    // get the keys for each item
-    var keys = Object.keys(this.storage);
-    // convert the keys to numbers
-    var numKeys = keys.map(function(key){
-      return parseInt(key);
-    });
-    // find the min number among the keys - the key of the first item
-    var firstKey = numKeys.reduce(function(min, key){
-      if (key < min) {
-        min = key;
-      }
-      return min;
-    });
-    var firstValue = this.storage[firstKey.toString()];
-    // debugger;
-    // store the value of the first item
-    var dequeuedValue = firstValue;
-    // debugger;
-    // delete the key of the first item, thus removing it
-    delete this.storage[firstKey.toString()];
-    // debugger;
-    // return the item that was removed
-    return dequeuedValue;
+  if (this.size() !== 0) {
+    var dequeuedItem = this.storage[this.startIndex];
+    delete this.storage[this.startIndex];
+    this.startIndex++;
+    return dequeuedItem;
   }
 };
 
 // size() - Return the number of items in the queue
 queueMethods.size = function() {
-  // get the keys of the items in the queue
-  var keys = Object.keys(this.storage);
-  // return the length of the array of keys (which is the number of items currently in the queue)
-  return keys.length;
+  return this.nextIndex - this.startIndex;
 };
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -3,7 +3,8 @@ var Stack = function() {
   var newStack = {};
   // add a storage object to hold int keys + items
   newStack.storage = {};
-  // use for alternate solution --> newStack.count = 0; update all THIS to lowercase
+  // keep track of count of items in the stack
+  newStack.stackCount = 0;
   // add the methods to the new stack object
   _.extend(newStack, stackMethods);
   // return the new stack object
@@ -15,83 +16,24 @@ var stackMethods = {};
 
 // add the methods to the methods object
 stackMethods.push = function(value) {
-  // check if there are any items in the stack
-  if (Object.keys(this.storage).length !== 0) {
-    // get the keys for each item
-    var keys = Object.keys(this.storage);
-    // convert the keys to numbers
-    var numKeys = keys.map(function(key){
-      return parseInt(key);
-    });
-    // find the max number among the keys - the key of the top item
-    var lastKey = numKeys.reduce(function(max, key){
-      if (key > max) {
-        max = key;
-      }
-      return max;
-    });
-    // increment the last key to get the new key
-    var newKey = lastKey + 1;
-  } else {
-    // if there were no items in the stack, the first key will be 1
-    var newKey = 1;
-  }
-  // set the new key to the value
-  this.storage[newKey.toString()] = value;
-
-  // alternate solution with count
-  // THIS.count++;
-  // THIS.storage[THIS.count] = value;
+  this.stackCount++;
+  this.storage[this.stackCount] = value;
 };
 
 stackMethods.pop = function() {
-  // check if there are any items in the stack
-  if (Object.keys(this.storage).length !== 0) {
-    // get the keys for each item
-    var keys = Object.keys(this.storage);
-    // convert the keys to numbers
-    var numKeys = keys.map(function(key){
-      return parseInt(key);
-    });
-    // find the max number among the keys - the key of the top item
-    var lastKey = numKeys.reduce(function(max, key) {
-      if (key > max) {
-        max = key;
-      }
-      return max;
-    });
-    // get the value of the top item
-    var lastValue = this.storage[lastKey.toString()];
+  if (this.stackCount > 0) {
     // store the value of the top item
-    var poppedValue = lastValue;
+    var poppedItem = this.storage[this.stackCount];
     // delete the key of the top item, thus removing it
-    delete this.storage[lastKey.toString()];
-    // return the item that was removed
-    return poppedValue;
+    delete this.storage[this.stackCount];
+    // decrement count
+    this.stackCount--;
+    return poppedItem;
   }
-
-  // alternate solution with count
-  // if (THIS.count > 0) {
-  //   // get the value of the top item
-  //   var lastValue = THIS.storage[THIS.count];
-  //   // store the value of the top item
-  //   var poppedValue = lastValue;
-  //   // delete the key of the top item, thus removing it
-  //   delete THIS.storage[THIS.count];
-  //   // decrement
-  //   THIS.count--;
-  //   return poppedValue;
-  // }
 };
 
 stackMethods.size = function() {
-  // get the keys of the items in the stack
-  var keys = Object.keys(this.storage);
-  // return the length of the array of keys (which is the number of items currently in the stack)
-  return keys.length;
-
-  // alternate solution with count
-  // return THIS.count;
+  return this.stackCount;
 };
 
 
