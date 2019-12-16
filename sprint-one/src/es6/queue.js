15201f198b4d77f10210a547ef7001b586c41e39
Pamela Sabella <psabella@Pamelas-Mac.local>
2019-12-16 04:01:24 -0800

    complete es6 stack and queue

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,7 +1,30 @@
 class Queue {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.startIndex = 0;
+    this.nextIndex = 0;
   }
 
+  // enqueue(string) - Add a string to the back of the queue
+  enqueue(value) {
+    // new value is set at nextIndex in storage
+    this.storage[this.nextIndex] = value;
+    this.nextIndex++;
+  }
+
+  // dequeue() - Remove and return the string at the front of the queue
+  dequeue() {
+  // check if there are any items in the queue
+    if (this.size() !== 0) {
+      var dequeuedItem = this.storage[this.startIndex];
+      delete this.storage[this.startIndex];
+      this.startIndex++;
+      return dequeuedItem;
+    }
+  }
+
+  // size() - Return the number of items in the queue
+  size() {
+    return this.nextIndex - this.startIndex;
+  }
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,7 +1,27 @@
 class Stack {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.stackCount = 0;
   }
 
+  push(value) {
+    this.stackCount++;
+    this.storage[this.stackCount] = value;
+  }
+
+  pop() {
+    if (this.stackCount > 0) {
+      // store the value of the top item
+      var poppedItem = this.storage[this.stackCount];
+      // delete the key of the top item, thus removing it
+      delete this.storage[this.stackCount];
+      // decrement count
+      this.stackCount--;
+      return poppedItem;
+    }
+  }
+
+  size() {
+    return this.stackCount;
+  }
 }
