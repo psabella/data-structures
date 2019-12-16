bc1be871467e1fbf2b47416cf844872009ecfeb6
Pamela Sabella <psabella@Pamelas-Mac.local>
2019-12-16 03:46:59 -0800

    complete pseudoclassical stack and queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,30 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.startIndex = 0;
+  this.nextIndex = 0;
+};
+
+// enqueue(string) - Add a string to the back of the queue
+Queue.prototype.enqueue = function(value) {
+  // new value is set at nextIndex in storage
+  this.storage[this.nextIndex] = value;
+  this.nextIndex++;
+};
+
+// dequeue() - Remove and return the string at the front of the queue
+Queue.prototype.dequeue = function() {
+  // check if there are any items in the queue
+  if (this.size() !== 0) {
+    var dequeuedItem = this.storage[this.startIndex];
+    delete this.storage[this.startIndex];
+    this.startIndex++;
+    return dequeuedItem;
+  }
+};
+
+// size() - Return the number of items in the queue
+Queue.prototype.size = function() {
+  return this.nextIndex - this.startIndex;
 };
 
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,25 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.stackCount = 0;
 };
 
+Stack.prototype.push = function(value){
+  this.stackCount++;
+  this.storage[this.stackCount] = value;
+};
 
+Stack.prototype.pop = function (){
+  if (this.stackCount > 0) {
+    // store the value of the top item
+    var poppedItem = this.storage[this.stackCount];
+    // delete the key of the top item, thus removing it
+    delete this.storage[this.stackCount];
+    // decrement count
+    this.stackCount--;
+    return poppedItem;
+  }
+};
+
+Stack.prototype.size = function() {
+  return this.stackCount;
+};
