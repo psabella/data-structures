b616d023e10b3b28cff583150b9694468b84104e
Pamela Sabella <psabella@Pamelas-Mac.local>
2019-12-16 03:31:58 -0800

    complete prototypal queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,34 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var newQueue = Object.create(queueMethods);
+  newQueue.storage = {};
+  newQueue.startIndex = 0;
+  newQueue.nextIndex = 0;
+  return newQueue;
 };
 
 var queueMethods = {};
+
+// enqueue(string) - Add a string to the back of the queue
+queueMethods.enqueue = function(value) {
+  // new value is set at nextIndex in storage
+  this.storage[this.nextIndex] = value;
+  this.nextIndex++;
+};
+
+// dequeue() - Remove and return the string at the front of the queue
+queueMethods.dequeue = function() {
+  // check if there are any items in the queue
+  if (this.size() !== 0) {
+    var dequeuedItem = this.storage[this.startIndex];
+    delete this.storage[this.startIndex];
+    this.startIndex++;
+    return dequeuedItem;
+  }
+};
+
+// size() - Return the number of items in the queue
+queueMethods.size = function() {
+  return this.nextIndex - this.startIndex;
+};
 
 
